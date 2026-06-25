b204cf894c76e20b4e73c8cf3de9bb39899ce14b
Chayanon Kongnual <692110130@cmu.ac.th>
2026-06-25 15:28:21 +0700

    Add initial VSCode configuration and sample C program

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("สวัสดีครับ ผม ชญานนท์ คงนวล\n");
+    printf("รหัสนักศึกษา: 692110130\n");
+    printf("วิชา: DG111 Computer Programming\n");
+    return 0;
+}
